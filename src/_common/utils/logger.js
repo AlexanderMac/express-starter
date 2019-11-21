95050f9bc21ea8140dea35f37e3fde7f829757cd
AlexanderMac <amatsibarov@gmail.com>
2019-11-21 21:23:58 +0300

    update winston logger to v3

--- src/_common/utils/logger.js
+++ src/_common/utils/logger.js
@@ -1,17 +1,34 @@
-'use strict';
-
-const _       = require('lodash');
+const _ = require('lodash');
 const winston = require('winston');
-const config  = require('../../../config/environment');
+const config = require('../../../config/environment');
 
-const transports = _.map(config.get('winston:transports'), (transportOpts, transportName) => {
-  switch (transportName) {
-    case 'console':
-      return new (winston.transports.Console)(transportOpts);
-    default:
-      throw new Error(`Invalid transport name: ${transportName}`);
-  }
+const { combine, colorize, simple } = winston.format;
+
+function customFormat() {
+  return combine(colorize(), simple());
+}
+
+let logger = winston.createLogger({
+  exitOnError: false,
+  transports: _.map(config.get('winston:transports'), (opts, transType) => {
+    switch (transType) {
+      case 'console':
+        return new winston.transports.Console({
+          level: opts.level,
+          format: customFormat()
+        });
+      default:
+        throw new Error('Unknown logger transport type: ' + transType);
+    }
+  })
 });
 
+logger.error = message => {
+  if (message instanceof Error) {
+    logger.log({ level: 'error', message: message.stack });
+  } else {
+    logger.log({ level: 'error', message });
+  }
+};
 
-module.exports = new (winston.Logger)({ transports });
+module.exports = logger;
