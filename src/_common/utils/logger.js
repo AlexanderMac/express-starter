f465e27723748bf6440cedfa41f11b456d33f37c
AlexanderMac <amatsibarov@gmail.com>
2020-05-29 12:29:11 +0300

    improve error processing in logger

--- src/_common/utils/logger.js
+++ src/_common/utils/logger.js
@@ -4,31 +4,30 @@ const config = require('../../../config/environment');
 
 const { combine, colorize, simple } = winston.format;
 
-function customFormat() {
-  return combine(colorize(), simple());
-}
+const errorFormat = winston.format(info => {
+  if (info.message instanceof Error) {
+    info.message = info.message.stack;
+  }
+
+  return info;
+});
 
 let logger = winston.createLogger({
   exitOnError: false,
   transports: _.map(config.get('winston:transports'), (opts, transType) => {
+    opts = _.cloneDeep(opts);
     switch (transType) {
       case 'console':
-        return new winston.transports.Console({
-          level: opts.level,
-          format: customFormat()
-        });
+        opts.format = combine(
+          errorFormat(),
+          colorize(),
+          simple()
+        );
+        return new winston.transports.Console(opts);
       default:
         throw new Error('Unknown logger transport type: ' + transType);
     }
   })
 });
-
-logger.error = message => {
-  if (message instanceof Error) {
-    logger.log({ level: 'error', message: message.stack });
-  } else {
-    logger.log({ level: 'error', message });
-  }
-};
 
 module.exports = logger;
